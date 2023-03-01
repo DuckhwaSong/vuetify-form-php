9a3dd4871fe30367eb267e72849977013ecbcd5e
Duckhwa Song <kf80song@gmail.com>
2023-03-01 17:06:03 +0900

    vue 소스 일부 추가

---
+++ components/myComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">Hello {{who}}</div>
+</template>
+
+<script>
+alert('q');
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
